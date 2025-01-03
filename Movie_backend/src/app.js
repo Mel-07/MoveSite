a478a6868e42efefd40b39ddb205e8cb56dc52a1
Mel-07 <emekaokocha07@gmail.com>
2025-01-03 15:12:25 +0100

    setting db with alwaysdata

--- Movie_backend/src/app.js
+++ Movie_backend/src/app.js
@@ -38,6 +38,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
